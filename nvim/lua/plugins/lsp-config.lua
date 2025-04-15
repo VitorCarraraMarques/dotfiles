693e6526ad7d66a92678fbc82bcdea69844edef0
Vítor Carrara <softwarecarrara@gmail.com>
2025-04-15 16:25:28 -0300

    rust lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,92 +1,96 @@
-local enable = true;
+local enable = true
 
 if not enable then
-    return {}
+	return {}
 else
-    return {
-        {
-            "williamboman/mason.nvim",
-            config = function()
-                require("mason").setup()
-            end,
-        },
-        {
-            "williamboman/mason-lspconfig.nvim",
-            config = function()
-                require("mason-lspconfig").setup({
-                    ensure_installed = {
-                        "lua_ls",
-                        "pyright",
-                        "html",
-                        "gopls",
-                        "templ",
-                        "jsonls",
-                        "cssls",
-                        "tailwindcss",
-                        "ts_ls",
-                        "clangd",
-                        "htmx",
-                        "denols",
-                    },
-                })
-            end,
-        },
-        {
-            "neovim/nvim-lspconfig",
-            lazy = false,
-            config = function()
-                local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	return {
+		{
+			"williamboman/mason.nvim",
+			config = function()
+				require("mason").setup()
+			end,
+		},
+		{
+			"williamboman/mason-lspconfig.nvim",
+			config = function()
+				require("mason-lspconfig").setup({
+					ensure_installed = {
+						"lua_ls",
+						"pyright",
+						"html",
+						"gopls",
+						"templ",
+						"jsonls",
+						"cssls",
+						"tailwindcss",
+						"ts_ls",
+						"clangd",
+						"htmx",
+						"denols",
+						"rust_analyzer",
+					},
+				})
+			end,
+		},
+		{
+			"neovim/nvim-lspconfig",
+			lazy = false,
+			config = function()
+				local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-                local lspconfig = require("lspconfig")
-                lspconfig.lua_ls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.pyright.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.html.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.gopls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.templ.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.emmet_language_server.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.jsonls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.cssls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.tailwindcss.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.ts_ls.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.denols.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.clangd.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.htmx.setup({
-                    capabilities = capabilities,
-                })
-                lspconfig.jdtls.setup({
-                    capabilities = capabilities,
-                })
+				local lspconfig = require("lspconfig")
+				lspconfig.lua_ls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.pyright.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.html.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.gopls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.templ.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.emmet_language_server.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.jsonls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.cssls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.tailwindcss.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.ts_ls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.denols.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.clangd.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.htmx.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.jdtls.setup({
+					capabilities = capabilities,
+				})
+				lspconfig.harper_ls.setup({
+					capabilities = capabilities,
+				})
 
-                vim.diagnostic.config({ underline = false })
-                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-                vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-                vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-            end,
-        },
-    }
+				vim.diagnostic.config({ underline = false })
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			end,
+		},
+	}
 end
